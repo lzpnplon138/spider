e7c2aec566042767178e330e6cecfdffc1a95301
lzpnplon138 <laochuai1208924@163.com>
2021-03-11 23:10:44 +0800

    55

--- ArticleSpider/spiders/jobbole.py
+++ ArticleSpider/spiders/jobbole.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
+import re
+
 import scrapy
+from scrapy.http import Request
+from urllib import parse
 
 
 class JobboleSpider(scrapy.Spider):
@@ -12,5 +16,29 @@ class JobboleSpider(scrapy.Spider):
         1.获取文章列表页中的文章url并交给scrapy下载后并进行解析
         2.获取下一页的url并交给scrapy进程下载, 下载完成后交给parse
         """
-        post_urls = response.css('.floated-thumb .post-thumb a')
-        pass
+        post_urls = response.css('#archive .floated-thumb .post-thumb a::attr(href)').extract()
+        for post_url in post_urls:
+            yield Request(url=parse.urljoin(response.url, post_url), callback=self.parse_detail)
+
+    def parse_detail(self, response):
+
+        # 提取文章的具体字段
+        title = response.xpath('//div[@class="entry-header"]/h1/text()').extract_first("")
+        create_date = response.xpath("//p[@class='entry-meta-hide-on-mobile']/text()").extract()[0].strip().replace("·",
+                                                                                                                    "").strip()
+        praise_nums = response.xpath("//span[contains(@class, 'vote-post-up')]/h10/text()").extract()[0]
+        fav_nums = response.xpath("//span[contains(@class, 'bookmark-btn')]/text()").extract()[0]
+        match_re = re.match(".*?(\d+).*", fav_nums)
+        if match_re:
+            fav_nums = match_re.group(1)
+
+        comment_nums = response.xpath("//a[@href='#article-comment']/span/text()").extract()[0]
+        match_re = re.match(".*?(\d+).*", comment_nums)
+        if match_re:
+            comment_nums = match_re.group(1)
+
+        content = response.xpath("//div[@class='entry']").extract()[0]
+
+        tag_list = response.xpath("//p[@class='entry-meta-hide-on-mobile']/a/text()").extract()
+        tag_list = [element for element in tag_list if not element.strip().endswith("评论")]
+        tags = ",".join(tag_list)
