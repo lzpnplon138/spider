e8e8b246b88fd228f4d43d9343994dd1ffb33ea0
lzpnplon138 <laochuai1208924@163.com>
2021-03-12 19:49:38 +0800

    url

--- ArticleSpider/spiders/jobbole.py
+++ ArticleSpider/spiders/jobbole.py
@@ -9,7 +9,7 @@ from urllib import parse
 class JobboleSpider(scrapy.Spider):
     name = 'jobbole'
     allowed_domains = ['blog.jobbole.com']
-    start_urls = ['http://blog.jobbole.com/']
+    start_urls = ['http://blog.jobbole.com/all-posts/']
 
     def parse(self, response):
         """
@@ -20,25 +20,34 @@ class JobboleSpider(scrapy.Spider):
         for post_url in post_urls:
             yield Request(url=parse.urljoin(response.url, post_url), callback=self.parse_detail)
 
+        # 提取下一页并交给scrapy下载
+        next_urls = response.css('.next.page-numbers::attr(href)').extract_first()
+        if next_urls:
+            yield Request(url=parse.urljoin(response.url, next_urls), callback=self.parse)
+
+
     def parse_detail(self, response):
 
         # 提取文章的具体字段
-        title = response.xpath('//div[@class="entry-header"]/h1/text()').extract_first("")
-        create_date = response.xpath("//p[@class='entry-meta-hide-on-mobile']/text()").extract()[0].strip().replace("·",
-                                                                                                                    "").strip()
-        praise_nums = response.xpath("//span[contains(@class, 'vote-post-up')]/h10/text()").extract()[0]
-        fav_nums = response.xpath("//span[contains(@class, 'bookmark-btn')]/text()").extract()[0]
+        title = response.css(".entry-header h1::text").extract()[0]
+        create_date = response.css("p.entry-meta-hide-on-mobile::text").extract()[0].strip().replace("·","").strip()
+        praise_nums = response.css(".vote-post-up h10::text").extract()[0]
+        fav_nums = response.css(".bookmark-btn::text").extract()[0]
         match_re = re.match(".*?(\d+).*", fav_nums)
         if match_re:
-            fav_nums = match_re.group(1)
+            fav_nums = int(match_re.group(1))
+        else:
+            fav_nums = 0
 
-        comment_nums = response.xpath("//a[@href='#article-comment']/span/text()").extract()[0]
+        comment_nums = response.css("a[href='#article-comment'] span::text").extract()[0]
         match_re = re.match(".*?(\d+).*", comment_nums)
         if match_re:
-            comment_nums = match_re.group(1)
+            comment_nums = int(match_re.group(1))
+        else:
+            comment_nums = 0
 
-        content = response.xpath("//div[@class='entry']").extract()[0]
+        content = response.css("div.entry").extract()[0]
 
-        tag_list = response.xpath("//p[@class='entry-meta-hide-on-mobile']/a/text()").extract()
+        tag_list = response.css("p.entry-meta-hide-on-mobile a::text").extract()
         tag_list = [element for element in tag_list if not element.strip().endswith("评论")]
         tags = ",".join(tag_list)
