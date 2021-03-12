4098f867c3dcf851b99bf4a95569722ebfa9ea20
lzpnplon138 <laochuai1208924@163.com>
2021-03-13 00:12:23 +0800

    pipeline

--- ArticleSpider/spiders/jobbole.py
+++ ArticleSpider/spiders/jobbole.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 import re
+import datetime
 
 import scrapy
 from scrapy.http import Request
 from urllib import parse
+
+from ArticleSpider.items import JobBoleArticleItem
 
 
 class JobboleSpider(scrapy.Spider):
@@ -16,21 +19,24 @@ class JobboleSpider(scrapy.Spider):
         1.获取文章列表页中的文章url并交给scrapy下载后并进行解析
         2.获取下一页的url并交给scrapy进程下载, 下载完成后交给parse
         """
-        post_urls = response.css('#archive .floated-thumb .post-thumb a::attr(href)').extract()
-        for post_url in post_urls:
-            yield Request(url=parse.urljoin(response.url, post_url), callback=self.parse_detail)
+        post_nodes = response.css('#archive .floated-thumb .post-thumb a')
+        for post_node in post_nodes:
+            image_url = post_node.css('img::attr(src)').extract_first('')
+            post_url = post_node.css('::attr(href)').extract_first('')
+            yield Request(url=parse.urljoin(response.url, post_url), meta={'front_image_url': image_url},
+                          callback=self.parse_detail)
 
         # 提取下一页并交给scrapy下载
         next_urls = response.css('.next.page-numbers::attr(href)').extract_first()
         if next_urls:
             yield Request(url=parse.urljoin(response.url, next_urls), callback=self.parse)
 
-
     def parse_detail(self, response):
-
+        article_item = JobBoleArticleItem()
         # 提取文章的具体字段
+        front_image_url = response.meta.get('front_image_url', '')  # 文章封面图
         title = response.css(".entry-header h1::text").extract()[0]
-        create_date = response.css("p.entry-meta-hide-on-mobile::text").extract()[0].strip().replace("·","").strip()
+        create_date = response.css("p.entry-meta-hide-on-mobile::text").extract()[0].strip().replace("·", "").strip()
         praise_nums = response.css(".vote-post-up h10::text").extract()[0]
         fav_nums = response.css(".bookmark-btn::text").extract()[0]
         match_re = re.match(".*?(\d+).*", fav_nums)
@@ -51,3 +57,17 @@ class JobboleSpider(scrapy.Spider):
         tag_list = response.css("p.entry-meta-hide-on-mobile a::text").extract()
         tag_list = [element for element in tag_list if not element.strip().endswith("评论")]
         tags = ",".join(tag_list)
+
+        article_item["title"] = title
+        article_item["url"] = response.url
+        try:
+            create_date = datetime.datetime.strptime(create_date, "%Y/%m/%d").date()
+        except Exception as e:
+            create_date = datetime.datetime.now().date()
+        article_item["create_date"] = create_date
+        article_item["front_image_url"] = [front_image_url]
+        article_item["praise_nums"] = praise_nums
+        article_item["comment_nums"] = comment_nums
+        article_item["fav_nums"] = fav_nums
+        article_item["tags"] = tags
+        article_item["content"] = content
