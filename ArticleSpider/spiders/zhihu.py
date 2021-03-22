380983b999faaab35fecf7801f6df4b38de14c00
lzpnplon138 <laochuai1208924@163.com>
2021-03-23 00:38:18 +0800

    知乎模拟登陆 success

--- ArticleSpider/spiders/zhihu.py
+++ ArticleSpider/spiders/zhihu.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import re
 import time
+import datetime
 from PIL import Image
 from urllib import parse
 import json
@@ -14,6 +15,9 @@ class ZhihuSpider(scrapy.Spider):
     name = 'zhihu'
     allowed_domains = ['www.zhihu.com']
     start_urls = ['http://www.zhihu.com/']
+
+    # question的第一页answer的请求url
+    start_answer_url = 'https://www.zhihu.com/api/v4/questions/{0}/answers?include=data%5B*%5D.is_normal%2Cadmin_closed_comment%2Creward_info%2Cis_collapsed%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Cis_sticky%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Ccreated_time%2Cupdated_time%2Creview_info%2Cquestion%2Cexcerpt%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%2Cupvoted_followees%3Bdata%5B*%5D.mark_infos%5B*%5D.url%3Bdata%5B*%5D.author.follower_count%2Cbadge%5B%3F(type%3Dbest_answerer)%5D.topics&offset={2}&limit={1}&sort_by=default'
 
     headers = {
         'Host': 'www.zhihu.com',
@@ -83,9 +87,13 @@ class ZhihuSpider(scrapy.Spider):
                 request_url = match_obj.group(1)
                 question_id = match_obj.group(2)
 
-                yield scrapy.Request(request_url, headers=self.headers, meta={'question_id': int(question_id)}, callback=self.parse_question)
+                yield scrapy.Request(request_url, headers=self.headers, meta={'question_id': int(question_id)},
+                                     callback=self.parse_question)
+            else:
+                yield scrapy.Request(url, headers=self.headers, callback=self.parse)
 
     def parse_question(self, response):
+        """处理问题函数"""
         item_loader = ItemLoader(item=ZhihuQuestionItem(), response=response)
         item_loader.add_value('zhihu_id', response.meta.get('question_id', ''))
         item_loader.add_css('title', 'h1.QuestionHeader-title::text')
@@ -99,4 +107,33 @@ class ZhihuSpider(scrapy.Spider):
 
         question_item = item_loader.load_item()
 
+        yield scrapy.Request(self.start_answer_url.format(response.meta.get('question_id', ''), 20, 0),
+                             headers=self.headers, callback=self.parse_answer)
+
         yield question_item
+        # 此处也需要对页面进行爬取, 同parse函数
+
+    def parse_answer(self, response):
+        """处理问题的回答, 返回json数据"""
+        answers = json.loads(response.text)
+        is_end = answers['paging']['is_end']
+        next_url = answers['paging']['next']
+
+        # 提取answer的具体结构
+        for answer in answers['data']:
+            answer_item = ZhihuAnswerItem()
+            answer_item['zhihu_id'] = answer['id']
+            answer_item['url'] = answer['url']
+            answer_item['question_id'] = answer['question']['id']
+            answer_item['author_id'] = answer['author']['id'] if 'id' in answer['author'] else None
+            answer_item['content'] = answer['content'] if 'content' in answer else None
+            answer_item['praise_num'] = answer['voteup_count']
+            answer_item['comments_num'] = answer['comment_count']
+            answer_item['create_time'] = answer['created_time']
+            answer_item['update_time'] = answer['updated_time']
+            answer_item['crawl_time'] = datetime.datetime.now()
+
+            yield answer_item
+
+        if not is_end:
+            yield scrapy.Request(next_url, headers=self.headers, callback=self.parse_answer)
